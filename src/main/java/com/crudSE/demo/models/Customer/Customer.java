9b66b698bf3aabca55009578f3ef279a7a122a95
Laya Pandey <layapandey7171@gmail.com>
2025-08-23 13:08:15 +0545

    customer.java file updated

--- src/main/java/com/crudSE/demo/models/Customer/Customer.java
+++ src/main/java/com/crudSE/demo/models/Customer/Customer.java
@@ -9,5 +9,16 @@ import java.util.List;
 @Entity
 @Data
 public class Customer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String email;
+  private String password;
+  private String address;
+  private Long phone;
+  
+  @OneToMany(mappedBy = "customer")
+  private List<OrderList> orderList;
 
 }
