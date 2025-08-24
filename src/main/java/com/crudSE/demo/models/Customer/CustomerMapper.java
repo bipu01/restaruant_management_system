11d089104012af2eb5c8bcfb2bb337a1bfed4f20
Laya Pandey <layapandey7171@gmail.com>
2025-08-24 10:40:56 +0545

    ADD: Customer Mapper updated

--- src/main/java/com/crudSE/demo/models/Customer/CustomerMapper.java
+++ src/main/java/com/crudSE/demo/models/Customer/CustomerMapper.java
@@ -3,5 +3,23 @@ package com.crudSE.demo.models.Customer;
 import com.crudSE.demo.DTOs.CustomerDTO;
 
 public class CustomerMapper {
+    public static CustomerDTO mapToCustomerDTO(Customer customer){
+    return new CustomerDTO(
+        customer.getId(),
+        customer.getName(),
+        customer.getEmail(),
+        customer.getAddress(),
+        customer.getPhone()
+    );
+  }
 
+  public static Customer mapToCustomer(CustomerDTO customerDTO){
+    return new Customer(
+        customerDTO.getId(),
+        customerDTO.getName(),
+        customerDTO.getEmail(),
+        customerDTO.getAddress(),
+        customerDTO.getPhone()
+    );
+  }
 }
